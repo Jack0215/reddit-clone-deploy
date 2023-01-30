df819fa9b8c29fffbe700135e8d9429ccedcc508
Jack <jack.lee@aimatics.ai>
2023-01-30 17:44:09 +0900

    change > ecosystem > script

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'reddit-server',
-      script: 'env-cmd -f .env.production ts-node ./src/server.ts',
+      script: 'npm run start:prod',
     },
   ],
 };
